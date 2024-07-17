2850470e6f5f7669d07460964bfcca43577fe7b5
Denis Sousa <denis.sousa@aluno.uece.br>
2024-07-17 10:34:27 -0300

    in progress

--- java-install.sh
+++ java-install.sh
@@ -4,6 +4,11 @@ sudo apt-get install trash-cli -y &&
 sudo apt-get install maven -y &&
 sudo apt-get install openjdk-8-jdk -y &&
 sudo apt purge openjdk-11-* -y &&
+udo apt install python3-pip &&
+sudo apt install python3-pip && 
 python3 -m venv env &&
 source env/bin/activate &&
-pip install -r requirements.txt
+pip3 install -r requirements.txt
+
+# sudo apt install python3-pip
+# only python3 main_grid.py works
